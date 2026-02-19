7dc4298c4ede518dafe27e15c0647f016278ff5c
ExostFlash <exostflash@mecdu.dev>
2026-02-19 14:40:33 +0100

    modif cita

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
-import { provideRouter, TitleStrategy, withHashLocation } from '@angular/router';
+import { provideRouter, TitleStrategy } from '@angular/router';
 import { Title } from '@angular/platform-browser';
 
 import { routes } from './app.routes';
@@ -9,7 +9,7 @@ import { AppTitleStrategy } from './app-title.strategy';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes, withHashLocation()),
+    provideRouter(routes),
     provideHttpClient(),
     Title,
     { provide: TitleStrategy, useClass: AppTitleStrategy }
